737066697f5bf3d05261feba93f60072299be91f
Alexander Poole <alex.o.poole@gmail.com>
2016-09-05 16:19:20 +0200

    Made a hello world class

--- Lab2/simple_proxy.py
+++ Lab2/simple_proxy.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python
 
+import socket
+import select
+import time
+import sys
+
+class My_Server :
+	def my_print(self):
+		return 'hello world' 
+output = My_Server()
+print (output.my_print())
