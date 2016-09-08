5a1d6edaa8b2b1c08cc1e06379f7914486b657a5
MiltonJohansson <miljo274@student.liu.se>
2016-09-08 14:43:00 +0200

    Lyckats connecta med webserver och skickat tillbaka url-en till clienten

--- Lab2/simple_proxy.py
+++ Lab2/simple_proxy.py
@@ -16,7 +16,7 @@ else:
 ######## Definierar main loop ###########
 def main():
 	# The server socket listens to the browser
-	print('Listening on port:', str(PORT))
+	print('\n[*] Listening on port:', str(PORT))
 	server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	try:
@@ -24,32 +24,46 @@ def main():
 		server_socket.listen(1)
 	except socket.error as msg:
 		server_socket.close()
-		server_socket = None
-		print('Error could not open socket')
+		print("\n[*] Error could not open server socket")
+		sys.exit(2)
 
 
 	conn, addr = server_socket.accept()
 	print('Connected by, ', addr)
 	server_socket.close()
-	data = ''
+	data_str = ''
 
 	# Getting url from get request
-	data_temp = conn.recv(BUFFER_SIZE)
-	data += str(data_temp,"utf-8")
-	conn.close()
+	data_byte = conn.recv(BUFFER_SIZE)
+	data_str += str(data_byte,"utf-8")
 
-	url_request = find_url(data)
+
+	url_request = find_url(data_str)
 
 	print(url_request)
-#	client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#
-#	client_socket.connect((url_request,80))
-#	client_socket.send("GET / HTTP/1.0%s" % (CRLF))
-#
-#	while 1:
-#		data_temp = client_socket.recv(BUFFER_SIZE)
-#		conn.send(data_temp)
-#		if not data_temp:break
+	try:
+		client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		client_socket.connect(('expressen.se',80))
+		client_socket.send(data_byte)
+		while 1:
+			print("In while-loop")
+			data_reply = client_socket.recv(BUFFER_SIZE)
+			if (len(data_reply)>0):
+				data_reply_str =str(data_reply,"utf-8" )
+				print(data_reply_str)
+				conn.send(data_reply)
+			else: break
+		print("after while-loop")
+		client_socket.close()
+		conn.close()
+	except socket.error as msg:
+		client_socket.close()
+		print("\n[*] Error could not open client socket")
+		sys.exit(2)
+
+		data_reply_str=''
+
+
 #
 #	conn.close()
 #	client_socket.close()
