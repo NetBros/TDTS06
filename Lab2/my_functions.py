7bc2a6608d75d25b2905d48e5b950cb9f5e6b2d0
Alexander Poole <alex.o.poole@gmail.com>
2016-09-14 13:24:04 +0200

    Did shit

--- Lab2/my_functions.py
+++ Lab2/my_functions.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+import string
+
 def find_url(data):
 	host = "Host: "
 	index_host_end = data.find(host)+ len(host)
 	data = data[index_host_end:]
-	end_of_line = data.find("\n")
+	end_of_line = data.find("\\r\\n")
 	data = data[:end_of_line]
 
 	n = data.find("www.")
@@ -14,10 +16,6 @@ def find_url(data):
 		k = (n+len("www."))
 		url_request = data[k:]
 
-	url_request = url_request.replace(" ","")
-	url_request = url_request.replace("\n","")
-	url_request = url_request[:-1]
-
 	return url_request
 
 def get_request_close(get_request):
@@ -26,12 +24,13 @@ def get_request_close(get_request):
 	close = "close"
 
 	index_first_half = get_request.find(connection)+len(connection)
-	index_second_half = index_connection_end+len(Keep-Alive)
+	print (index_first_half)
+	#index_second_half = index_first_half+len(keep_alive)
 
-	first_half = get_request[index_first_half]
-	second_half = get_request[index_second_half:]
+	#first_half = get_request[index_first_half]
+	#second_half = get_request[index_second_half:]
 
-	return first_half+close+second_half
+	#return first_half+close+second_half
 
 def convert_to_ascii(text):
     return " ".join(str(ord(char)) for char in text)
--- Lab2/request_handler.py
+++ Lab2/request_handler.py
@@ -12,34 +12,43 @@ class request_handler(threading.Thread):
 
     def run(self):
         client_req = ''
-
+        server_req = ''
         get_request = self.conn.recv(self.BUFFER_SIZE)
         client_req += str(get_request)
+        #test = string(client_req)
 
         # Finding host address from get request
         host = find_url(client_req)
-        #print(get_request)
         #get_request = get_request_close(get_request)
         #print(get_request)
-        print(host)
+        #print(host)
 
         try:
             client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            print("[*] Setting upp connection to ", url_request)
-            client_socket.connect((host,80))
+            print("[*] Setting upp connection to ", host)
+            client_socket.connect(("ida.liu.se",80))
+            print("[*] Connected, sending get request")
             client_socket.send(get_request)
+            print("[*] Get request sent")
             while 1:
-                server_request = client_socket.recv(self.BUFFER_SIZE)
-                if (len(server_request)>0):
-                    #server_request += str(data_reply,"utf-8")
-                    self.conn.send(server_request)
-                else: break
+                print("[*] Prepering to peek at data")
+                peek_data = client_socket.recv(self.BUFFER_SIZE,socket.MSG_PEEK)
+                peek_bytes = len(peek_data)
+                print("Bytes in recive que",peek_bytes)
+
+                if(peek_bytes>self.BUFFER_SIZE):
+                    peek_bytes = self.BUFFER_SIZE
+                elif(peek_bytes == 0): break
+
+                server_request = client_socket.recv(peek_bytes)
+                #server_req += str(server_request,"utf-8")
+                print(server_request)
+                self.conn.send(server_request)
+
             client_socket.close()
             self.conn.close()
         except socket.error as msg:
-        	if client_socket:
-        		client_socket.close()
-        	if self.conn:
-        		self.conn.close()
+        	client_socket.close()
+        	self.conn.close()
         	print("\n[*] Error could not open client socket")
         	sys.exit(2)
