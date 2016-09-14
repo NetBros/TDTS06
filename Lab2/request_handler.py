d6348f59e8d72e7a53bc4071d94d251409aeba13
Alexander Poole <alex.o.poole@gmail.com>
2016-09-14 18:22:32 +0200

    Installed timer in while loop

--- Lab2/request_handler.py
+++ Lab2/request_handler.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import threading, socket, sys
+import threading, socket, sys, time
 from my_functions import *
 
 class request_handler(threading.Thread):
@@ -13,6 +13,7 @@ class request_handler(threading.Thread):
     def run(self):
         client_req = ''
         server_req = ''
+        timeout = 1
         get_request = self.conn.recv(self.BUFFER_SIZE)
         client_req += str(get_request)
         #test = string(client_req)
@@ -20,29 +21,31 @@ class request_handler(threading.Thread):
         # Finding host address from get request
         host = find_url(client_req)
         #get_request = get_request_close(get_request)
-        #print(get_request)
-        #print(host)
 
         try:
+            print("[*] I'm begining my work in a new thread" )
             client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
             print("[*] Setting upp connection to ", host)
-            client_socket.connect(("ida.liu.se",80))
-            print("[*] Connected, sending get request")
+            client_socket.connect((host,80))
             client_socket.send(get_request)
-            print("[*] Get request sent")
+
+            TIME_FLAGG = 0
             while 1:
-                print("[*] Prepering to peek at data")
                 peek_data = client_socket.recv(self.BUFFER_SIZE,socket.MSG_PEEK)
                 peek_bytes = len(peek_data)
-                print("Bytes in recive que",peek_bytes)
 
                 if(peek_bytes>self.BUFFER_SIZE):
                     peek_bytes = self.BUFFER_SIZE
-                elif(peek_bytes == 0): break
+                elif(peek_bytes == 0 and not TIME_FLAGG):
+                    TIME_FLAGG = 1
+                    start_time = time.time()
+                elif(peek_bytes == 0 and ((start_time + timeout) < time.time()) and TIME_FLAGG):
+                    break
+                elif(peek_bytes > 0 and TIME_FLAGG):
+                    TIME_FLAGG = 0
 
                 server_request = client_socket.recv(peek_bytes)
-                #server_req += str(server_request,"utf-8")
-                print(server_request)
+                server_req += str(server_request)
                 self.conn.send(server_request)
 
             client_socket.close()
@@ -50,5 +53,7 @@ class request_handler(threading.Thread):
         except socket.error as msg:
         	client_socket.close()
         	self.conn.close()
-        	print("\n[*] Error could not open client socket")
+        	print("\n[*] Error with socket in thread")
         	sys.exit(2)
+
+        print("[*] I have finished my work in this thread!")
