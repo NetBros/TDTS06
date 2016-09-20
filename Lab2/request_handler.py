2e13a98907fb4ae83a4c148bc70cf2ccc71ae6e6
Alexander Poole <alex.o.poole@gmail.com>
2016-09-20 17:41:05 +0200

    Made shit silky smooth

--- Lab2/request_handler.py
+++ Lab2/request_handler.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-
-import threading, socket, sys, time
 from my_functions import *
 from array import array
 
@@ -28,22 +26,22 @@ class request_handler(threading.Thread):
             temp_get = get_dict["GET"].split(" ")
             get_dict["GET"] = "http://www.ida.liu.se/~TDTS04/labs/2011/ass2/error1.html" + " " + temp_get[1]
 
-            [header, body] = server_conn(get_dict)
+        [header, body] = server_conn(get_dict,self.BUFFER_SIZE)
 
-            for key in get_dict:
-                if key == "Content-Type":
-                        if not -1 == header_dict["Content-Type"].find("text/html"):
-                            print("Content-Type find------------")
-                            try:
-                                check_ban(get_dict["GET"],1,self.ban_list)
-                            except My_Error as e:
-                                print("[*!*] ERROR found ",e.word," in url, redirecting")
-                                get_dict["Host"] = "www.ida.liu.se"
-                                temp_get = get_dict["GET"].split(" ")
-                                get_dict["GET"] = "http://www.ida.liu.se/~TDTS04/labs/2011/ass2/error2.html" + " " + temp_get[1]
-                                [header, body] = server_conn(get_dict)
+            #for key in get_dict:
+            #    if key == "Content-Type":
+            #            if not -1 == header_dict["Content-Type"].find("text/html"):
+            #                print("Content-Type find------------")
+            #                try:
+            #                    check_ban(get_dict["GET"],1,self.ban_list)
+            #                except My_Error as e:
+            #                    print("[*!*] ERROR found ",e.word," in url, redirecting")
+            #                    get_dict["Host"] = "www.ida.liu.se"
+            #                    temp_get = get_dict["GET"].split(" ")
+            #                    get_dict["GET"] = "http://www.ida.liu.se/~TDTS04/labs/2011/ass2/error2.html" + " " + temp_get[1]
+            #                    [header, body] = server_conn(get_dict)
 
-            server_request = header + body
+        server_request = header + body
 
-            self.conn.send(server_request)
-            self.conn.close()
+        self.conn.send(server_request)
+        self.conn.close()
