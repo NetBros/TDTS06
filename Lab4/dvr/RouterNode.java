99ce77e1edc28a4c49fce26a0fac47da5b150cd0
MiltonJohansson <miljo274@student.liu.se>
2016-10-07 09:49:31 +0200

    commented out stuff

--- Lab4/dvr/RouterNode.java
+++ Lab4/dvr/RouterNode.java
@@ -8,8 +8,8 @@ public class RouterNode {
 
   //--------------------------------------------------
   public RouterNode(int ID, RouterSimulator sim, int[] costs) {
-    myCosts = costs;
-    totalNodes = RouterSimulator.NUM_NODES;
+  //  myCosts = costs;
+    /*totalNodes = RouterSimulator.NUM_NODES;
     myForward = new int[totalNodes*2];
     for(int i=0; i<totalNodes; i++)
     {
@@ -19,6 +19,7 @@ public class RouterNode {
       else
         myForward[i] = i;
     }
+    */
     myID = ID;
     this.sim = sim;
     myGUI =new GuiTextArea("  Output window for Router #"+ ID + "  ");
@@ -30,7 +31,7 @@ public class RouterNode {
   //--------------------------------------------------
   public void recvUpdate(RouterPacket pkt) {
   // recive info about the connecting neighbours
-  for (int i=0; i< totalNodes ; i++)
+/*  for (int i=0; i< totalNodes ; i++)
     {
       costToRoute = myForward[pkt.sourceid+totalNodes]+pkt.mincosts[i+totalNodes];
       if (myForward[i+totalNodes]>(costToRoute){
@@ -39,13 +40,13 @@ public class RouterNode {
       }
     }
 // updating myForward depending on RouterPacket mincost
-
+*/
   }
 
 
   //--------------------------------------------------
   private void sendUpdate(RouterPacket pkt) {
-    pkt.mincosts = myForward;
+  //  pkt.mincosts = myForward;
     sim.toLayer2(pkt);
     //Update RouterPacket mincost with help from updated myForward
   }
@@ -55,7 +56,7 @@ public class RouterNode {
   public void printDistanceTable() {
   //Print the info about info we know about network so far
 	  myGUI.println("Current table for " + myID +
-			"  at time " + sim.getClocktime() + myForward);
+			"  at time " + sim.getClocktime() );//+ myForward);
   }
 
   //--------------------------------------------------
