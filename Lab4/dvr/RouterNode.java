c93879bf527895798f0abe2a18613bd37055f08e
Milton <miljo274@student.liu.se>
2016-11-01 16:59:34 +0100

    we can print stuff and trying to fix buggs regarding routs

--- Lab4/dvr/RouterNode.java
+++ Lab4/dvr/RouterNode.java
@@ -5,96 +5,169 @@ public class RouterNode {
   private GuiTextArea myGUI;
   private RouterSimulator sim;
   private int[] costs = new int[RouterSimulator.NUM_NODES];
-  private int[] myCosts;
   private int totalNodes;
-  private int[] myForward;
+  private int[][] myForwardTable = new int[RouterSimulator.NUM_NODES][RouterSimulator.NUM_NODES];
+  private int[] myForwardVia = new int[RouterSimulator.NUM_NODES];
 
   //--------------------------------------------------
   public RouterNode(int ID, RouterSimulator sim, int[] costs) {
-    myCosts = costs;
     totalNodes = RouterSimulator.NUM_NODES;
-    myForward = new int[totalNodes*2];
-    for(int i=0; i<totalNodes; i++)
-    {
-      myForward[i+totalNodes] = costs[i];
-      if (myCosts[i] == 999)
-        myForward[i] = 999;
-      else
-        myForward[i] = i;
-    }
-
     myID = ID;
     this.sim = sim;
     myGUI =new GuiTextArea("  Output window for Router #"+ ID + "  ");
-    sendUpdate();
     System.arraycopy(costs, 0, this.costs, 0, RouterSimulator.NUM_NODES);
-  }
+    for (int col=0;col<totalNodes ;col++ ) {
+      for (int row=0;row<totalNodes ;row++ ) {
+        if (row == col) {
+          myForwardTable[row][col] = 0;
+        }
+        else if (row == myID) {
+          myForwardTable[row][col] = 999;
+        }
+        else if (col == myID ){
+          myForwardTable[row][col] = costs[row];
+        }
+        else{
+          myForwardTable[row][col] = 999;
+        }
+        if (myForwardTable[row][myID]<999) {
+            myForwardVia[row] = row;
+        }
+        else{
+          myForwardVia[row] = 999;
+        }
+      }
+
+      }
+      update();
+      sendUpdate();
+    }
+
+  private boolean update(){
+    boolean updated = false;
+    for (int nodeWeWant2=0;nodeWeWant2<totalNodes ;nodeWeWant2++ ) {
+      int old_cost = myForwardTable[nodeWeWant2][myID];
+      int old_node = myForwardVia[nodeWeWant2];
+      for (int nodeWeWant3=0;nodeWeWant3<totalNodes ;nodeWeWant3++ ) {
+        if(myForwardVia[nodeWeWant3]<999){
+          int new_cost = myForwardTable[myForwardVia[nodeWeWant3]][myID] + myForwardTable[nodeWeWant3][nodeWeWant2];
+          //myGUI.println("cost from " + nodeWeWant3 + " to " + nodeWeWant2 +" is " + myForwardTable[nodeWeWant3][nodeWeWant2]);
+          //myGUI.println("cost from " + myID +" to " +nodeWeWant3+ " is " + myForwardTable[nodeWeWant3][myID]);
+          //myGUI.println("old_cost: "+old_cost);
+          //myGUI.println("new_cost: " +new_cost);
+        if ((new_cost<old_cost) || ((new_cost<999)&&(old_node == 999))) {
+          myGUI.println("Updating cost");
+          updated = true;
+          old_cost = new_cost;
+          old_node = nodeWeWant3;
+        }
+        }
+      }
+      myForwardTable[nodeWeWant2][myID] = old_cost;
+      myForwardVia[nodeWeWant2] = old_node;
+    }
+    return updated;
+    }
 
   //--------------------------------------------------
   public void recvUpdate(RouterPacket pkt) {
     myGUI.println("recv update");
-  // recive info about the connecting neighbours
-  boolean forwardFlagg = false;
-  for (int i=0; i< totalNodes ; i++){
-      int costToRoute = myForward[pkt.sourceid+totalNodes]+pkt.mincost[i+totalNodes];
-      if (myForward[i+totalNodes]>costToRoute){
-        if(myForward[pkt.sourceid] == pkt.sourceid){
-          myForward[i] = pkt.sourceid;
-        }
-          myForward[i] = myForward[pkt.sourceid];
-        myForward[i+totalNodes] = costToRoute;
-        forwardFlagg = true;
-      }
+    // updating myForward depending on RouterPacket mincost
+    for (int node=0;node<totalNodes ;node++ ) {
+     myForwardTable[node][pkt.sourceid] = pkt.mincost[node];
     }
-// updating myForward depending on RouterPacket mincost
-  if(forwardFlagg){
+  if(update()){
     sendUpdate();
   }
-
   }
 
 
   //--------------------------------------------------
   private void sendUpdate() {
-    myGUI.println("send update");
-    for (int i = 0;i<totalNodes ;i++ ) {
-      if ((costs[i] != 999 ) && (i != myID)) {
-        RouterPacket myPacket = new RouterPacket(myID,i,myForward);
-        myPacket.mincost = myForward;
+  myGUI.println("send update");
+    for (int nodeWeWant2=0;nodeWeWant2<totalNodes ;nodeWeWant2++ ) {
+      if((costs[nodeWeWant2] != 999) && (nodeWeWant2 != myID)){
+        int[] myMinCost = new int[totalNodes];
+        for (int node=0;node<totalNodes ;node++ ) {
+          if(myForwardVia[node] == nodeWeWant2){
+            myMinCost[node] = 999;
+          }
+          else{
+            myMinCost[node] = myForwardTable[node][myID];
+          }
+        }
+        RouterPacket myPacket = new RouterPacket(myID,nodeWeWant2,myMinCost);
         sim.toLayer2(myPacket);
       }
     }
+
+    }
     //Update RouterPacket mincost with help from updated myForward
-  }
 
 
   //--------------------------------------------------
+
   public void printDistanceTable() {
-  //Print the info about info we know about network so far
-	  myGUI.println("Current table for " + myID +
-			"  at time " + sim.getClocktime());
-    for (int i = 0;i<totalNodes ;i++ ) {
-      myGUI.println("Fastest route to " + i + " is through " +myForward[i] + " with cost " + myForward[i+totalNodes]);
+  	  myGUI.println("Current table for " + myID +
+  			"  at time " + sim.getClocktime());
+
+      String toPrint;
+
+      myGUI.println("\nDistancetable:");
+      printHeader();
+      toPrint = "\tcost\t|";
+      for (int j = 0;j<totalNodes ;j++ ) {
+        for(int i=0; i<RouterSimulator.NUM_NODES; i++){
+          toPrint += "\t" + myForwardTable[j][i];
+        }
+        toPrint += "\n\t\t|";
+      }
+      myGUI.println(toPrint);
+
+      myGUI.println("\nOur distance vector and routes:");
+      printHeader();
+      toPrint = "\tcost\t|";
+      for(int i=0; i<RouterSimulator.NUM_NODES; i++){
+        toPrint += "\t" + myForwardTable[i][myID];
+      }
+      myGUI.println(toPrint);
+
+      toPrint = "\troute\t|";
+      for(int i=0; i<RouterSimulator.NUM_NODES; i++){
+        if(myForwardVia[i] == RouterSimulator.INFINITY){
+          toPrint += "\t-";
+        }else{
+          toPrint += "\t" + myForwardVia[i];
+        }
+      }
+      myGUI.println(toPrint);
     }
-  }
+
+  //---------------help fuction to clean up print code -----------
+    private void printHeader() {
+      String toPrint;
+      toPrint = "\tdst\t|";
+      for(int i=0; i<RouterSimulator.NUM_NODES; i++){
+        toPrint += "\t" + i;
+      }
+      myGUI.println(toPrint);
+      toPrint = printLine(45 + 23*RouterSimulator.NUM_NODES);
+      myGUI.println(toPrint);
+
+    }
+    //---------help to print -----------------
+
+    private String printLine(int num){
+      String toPrint = "";
+      for(int i=0; i<num; i++){
+        toPrint += "-";
+      }
+      return toPrint;
+    }
 
   //--------------------------------------------------
   public void updateLinkCost(int dest, int newcost) {
 
-    myGUI.println("updateLinkCost");
-    costs[dest] = newcost;
-  // recive info about the connecting neighbours
-    bool forwardFlagg = false;
-    int oldCost = myForward[dest+totalNodes];
-    if(newcost < oldCost){
-      myForward[dest] = dest;
-      myForward[dest+totalNodes] = newcost;
-      forwardFlagg = true;
-    }
-    // updating myForward depending on RouterPacket mincost
-  if(forwardFlagg){
-    sendUpdate();
-  }
-  }
-
+  //  myGUI.println("updateLinkCost");
+}
 }
